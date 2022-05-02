0e8504b245cfbb961a50620fdda945902d8c4a0e
xVal4k <cristianbricicaru.cb@gmail.com>
2022-05-02 19:29:18 +0200

    some fixes

--- classes/User.php
+++ classes/User.php
@@ -4,12 +4,28 @@ require_once(__DIR__.'/Card.php');
 class User {
     private $name, $surname, $country, $address;
     
-    public function __construct(string $_name, string $_surname, string $_country, string $_address)
+    public function __construct(string $_name, string $_surname)
     {
         $this->name = $_name;
         $this->surname = $_surname;
+    }
+
+    public function setCountry($_country) {
         $this->country = $_country;
+        return $this;
+    }
+    
+    public function getCountry() {
+        return $this->country;
+    }
+
+    public function setAddress($_address) {
         $this->address = $_address;
+        return $this;
+    }
+    
+    public function getAddress() {
+        return $this->address;
     }
 }
 
@@ -17,23 +33,36 @@ class User {
 class RegisteredUser extends User {
     private $username, $email, $password, $birthDate, $discount = 0.20;
     
-    public function __construct(string $_name, string $_surname, string $_country, string $_address, string $_username, string $_email, string $_password, string $_birthDate) {
-        parent::__construct($_name, $_surname, $_country, $_address);
+    public function __construct(string $_name, string $_surname, string $_username, string $_password) {
+        parent::__construct($_name, $_surname);
 
         if (strlen($_username) < 6 || strlen($_username) > 15) {
             $this->usernameWrn = 'Username must be at least 6 and maximum 15 characters long';
         } else {
             $this->username = $_username;
         };
-
-        $this->email = $_email;
-
         if (strlen($_username) < 6 || strlen($_username) > 15) {
             $this->passwordWrn = 'Password must be at least 6 characters long';
         } else {
             $this->password = $_password;
         };
+    }
 
+    public function setEmail($_email) {
+        $this->email = $_email;
+        return $this;
+    }
+    
+    public function getEmail() {
+        return $this->email;
+    }
+
+    public function setBirthDate($_birthDate) {
         $this->birthDate = $_birthDate;
+        return $this;
+    }
+    
+    public function getBirthDate() {
+        return $this->birthDate;
     }
 }
--- index.php
+++ index.php
@@ -2,10 +2,14 @@
 require_once(__DIR__.'/classes/User.php');
 require_once(__DIR__.'/classes/Products.php');
 
-$user1 = new RegisteredUser('Cristian', 'Bricicaru', 'Italia', 'Via Busnelli 7, Saronno', 'CristiB.', 'C******b@gmail.com', '********', '02-04-2000');
+$user1 = new RegisteredUser('Cristian', 'Bricicaru', 'CristiB.', '********', );
+$user1->setCountry('Italia')->setAddress('Via Busnelli 7, Saronno')->setEmail('C******b@gmail.com')->setBirthDate('02-04-2000');
 $card_user1 = new Card('Cristian Bricicaru', '**** **** **** ****', "**-**", '***');
 $product1 = new Products("1", "Ciotola Gatto", "Accessori", "13.99");
+$product1->setProductInfo('Lorem ipsum dolor, sit amet consectetur adipisicing elit.');
 
 var_dump($user1);
 var_dump($card_user1);
 var_dump($product1);
+
+
